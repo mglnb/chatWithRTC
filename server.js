bb3be1fd682df9c189fad66f81cbecb89478ed50
Miguel Boanova <miguel.boanova@prodigious.com>
2017-08-28 09:25:08 -0300

    added server lines

--- server.js
+++ server.js
@@ -17,6 +17,101 @@ var WebSocketServer = require('websocket').server;
 var connectionArray = [];
 var nextID = Date.now();
 var appendToMakeUnique = 1;
+// Output logging information to console
+
+function log(text) {
+  var time = new Date();
+
+  console.log("[" + time.toLocaleTimeString() + "] " + text);
+}
+
+// If you want to implement support for blocking specific origins, this is
+// where you do it. Just return false to refuse WebSocket connections given
+// the specified origin.
+function originIsAllowed(origin) {
+  return true;    // We will accept all connections
+}
+
+// Scans the list of users and see if the specified name is unique. If it is,
+// return true. Otherwise, returns false. We want all users to have unique
+// names.
+function isUsernameUnique(name) {
+  var isUnique = true;
+  var i;
+
+  for (i=0; i<connectionArray.length; i++) {
+    if (connectionArray[i].username === name) {
+      isUnique = false;
+      break;
+    }
+  }
+  return isUnique;
+}
+
+// Sends a message (which is already stringified JSON) to a single
+// user, given their username. We use this for the WebRTC signaling,
+// and we could use it for private text messaging.
+function sendToOneUser(target, msgString) {
+  var isUnique = true;
+  var i;
+
+  for (i=0; i<connectionArray.length; i++) {
+    if (connectionArray[i].username === target) {
+      connectionArray[i].sendUTF(msgString);
+      break;
+    }
+  }
+}
+
+// Scan the list of connections and return the one for the specified
+// clientID. Each login gets an ID that doesn't change during the session,
+// so it can be tracked across username changes.
+function getConnectionForID(id) {
+  var connect = null;
+  var i;
+
+  for (i=0; i<connectionArray.length; i++) {
+    if (connectionArray[i].clientID === id) {
+      connect = connectionArray[i];
+      break;
+    }
+  }
+
+  return connect;
+}
+
+// Builds a message object of type "userlist" which contains the names of
+// all connected users. Used to ramp up newly logged-in users and,
+// inefficiently, to handle name change notifications.
+function makeUserListMessage() {
+  var userListMsg = {
+    type: "userlist",
+    users: []
+  };
+  var i;
+
+  // Add the users to the list
+
+  for (i=0; i<connectionArray.length; i++) {
+    userListMsg.users.push(connectionArray[i].username);
+  }
+
+  return userListMsg;
+}
+
+// Sends a "userlist" message to all chat members. This is a cheesy way
+// to ensure that every join/drop is reflected everywhere. It would be more
+// efficient to send simple join/drop messages to each user, but this is
+// good enough for this simple example.
+function sendUserListToAll() {
+  var userListMsg = makeUserListMessage();
+  var userListMsgStr = JSON.stringify(userListMsg);
+  var i;
+
+  for (i=0; i<connectionArray.length; i++) {
+    connectionArray[i].sendUTF(userListMsgStr);
+  }
+}
 
 var server = http.createServer(function (request, response) {
   console.log((new Date()) + " Received request for " + request.url);
